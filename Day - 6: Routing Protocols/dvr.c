dca0d69c2438ad5c66bc290eb14cf111a433867b
yadul <95083929+man-yadul@users.noreply.github.com>
2022-09-15 19:24:51 +0530

    Update dvr.c

--- Day - 6: Routing Protocols/dvr.c
+++ Day - 6: Routing Protocols/dvr.c
@@ -4,8 +4,7 @@ struct node
 {
     unsigned dist[20];
     unsigned from[20];
-}
-rt[10];
+} rt[10];
 int main()
 {
     int costmat[20][20];
@@ -23,17 +22,24 @@ int main()
             rt[i].from[j] = j;
         }
     }
-    do {
+    do
+    {
         count = 0;
         for (i = 0; i < nodes; i++)
-        for (j = 0; j < nodes; j++)
-            for (k = 0; k < nodes; k++)
-                if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j])
+        {
+            for (j = 0; j < nodes; j++)
+            {
+                for (k = 0; k < nodes; k++)
                 {
-                    rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
-                    rt[i].from[j] = k;
-                    count++;
+                    if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j])
+                    {
+                        rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
+                        rt[i].from[j] = k;
+                        count++;
+                    }
                 }
+            }
+        }
     } while (count != 0);
     for (i = 0; i < nodes; i++)
     {
